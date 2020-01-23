bcf40bc508ae8725c9d45bf50aa1cd1281b8b657
Alexander Xu Chen <alexxu.chen@mail.utoronto.ca>
2020-01-23 00:05:17 -0500

    combined website components in app

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowseRouter as Router, Route, Switch } from 'react-router-dom';
+
+import React from "react";
+import "./App.css";
+
+import Nav from "./components/navbar";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Footer from "./components/footer";
+// import Stars from "./components/stars";
 
 function App() {
   return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NoMatch} />
-
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <div className="App text-dark">
+      <Home />
+      <Nav>
+          <About />
+          <br></br>
+          <Projects />
+      </Nav>
+      <Footer></Footer>
+    </div>
   );
 }
 
