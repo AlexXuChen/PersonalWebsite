8a6561a7fdaf62c728161e5158159adb77f64e4b
Alexander Xu Chen <alexxu.chen@mail.utoronto.ca>
2020-01-24 01:42:29 -0500

    tweaked ids for insta creds

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,17 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-    <footer className="site-footer">
-      <div class="container">
-        <p id="nametag">© Copyright - Alexander Chen 2020</p>
-      </div>
-    </footer>
+      <footer className="site-footer">
+        <div class="container">
+          <p id="nametag">© Copyright - Alexander Chen 2020</p>
+          <p class="insta-tag">
+            Photo creds: @thethomasbui
+            <a href="https://www.instagram.com/thethomasbui/" class="btn">
+              <i class="fab fa-instagram text-light"></i>
+            </a>
+          </p>
+        </div>
+      </footer>
     );
   }
 }
